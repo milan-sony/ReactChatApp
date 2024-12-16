6c6ce551d28a73760e990069e050c518e2e7a01b
Milan Sony <milansonyofficial@gmail.com>
2024-12-16 15:26:27 +0530

    feat: user auth checked, loader added

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,22 @@ import React, { useEffect } from 'react'
 import Router from './router/Router'
 import Navbar from './components/Navbar/Navbar'
 import { useAuthStore } from './store/userAuthStore'
+import { Loader } from 'lucide-react'
 
 function App() {
-  const { authUser, checkAuth } = useAuthStore()
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore()
 
   useEffect(() => {
     checkAuth()
   }, [checkAuth])
 
   console.log("Auth user: ", authUser)
+
+  if (isCheckingAuth && !authUser) return (
+    <div className='flex items-center justify-center h-screen'>
+      <Loader className='size-10 animate-spin' />
+    </div>
+  )
 
   return (
     <div>
