defe31f8d5ae4c3f3c5a4c232c6166f1061ee73b
Milan Sony <milansonyofficial@gmail.com>
2024-12-17 10:37:34 +0530

    custom color added

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,6 @@ export default {
     extend: {
       colors: {
         "custblue": "#2185D5",
-        "custblack": "#303841",
-        "custgrey": "#3A4750",
-        "custwhite": "#F3F3F3"
       },
       fontFamily: {
         "Open-Sans": ["Open Sans", "sans-serif"]
