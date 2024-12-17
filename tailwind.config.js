1ff826e9bc045c9ae552a000299a36e76286020c
Milan Sony <milansonyofficial@gmail.com>
2024-12-17 10:31:10 +0530

    feat: cust colors and font added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,20 @@
-import daisyui from "daisyui"
-
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "custblue": "#2185D5",
+        "custblack": "#303841",
+        "custgrey": "#3A4750",
+        "custwhite": "#F3F3F3"
+      },
+      fontFamily: {
+        "Open-Sans": ["Open Sans", "sans-serif"]
+      }
+    },
   },
-  plugins: [daisyui],
+  plugins: [],
 }
