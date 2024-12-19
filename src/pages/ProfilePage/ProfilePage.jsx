ec3b733de56df2740cfb3c5fcd94664fb150965d
Milan Sony <milansonyofficial@gmail.com>
2024-12-19 14:43:04 +0530

    style: profile pg modifies

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -9,7 +9,7 @@ const ProfilePage = () => {
     };
 
     return (
-        <div className="pt-20">
+        <div className="h-screen">
             <div className="max-w-2xl mx-auto p-4 py-8">
                 <div className="bg-white shadow dark:bg-slate-900 rounded-xl p-6 space-y-8">
                     <div className="text-center">
