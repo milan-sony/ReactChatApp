0750ec4b26b7d6af1670b1ec8f3ed5a16265afa7
Milan Sony <milansonyofficial@gmail.com>
2024-12-24 18:14:06 +0530

    style: home page created

--- src/pages/HomePage/Homepage.jsx
+++ src/pages/HomePage/Homepage.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import { userChatStore } from '../../store/userChatStore'
+import Sidebar from '../../components/Sidebar/Sidebar'
+import NoChatSelected from '../../components/NoChatSelected/NoChatSelected'
+import ChatContainer from '../../components/ChatContainer/ChatContainer'
+
 
 function Homepage() {
+    const { selectedUser } = userChatStore()
+
     return (
-        <div>
-            <p>Home page</p>
-        </div>
+        <>
+            <div className='bg-custblue min-h-dvh'>
+                <div className='flex items-center justify-center pt-20 pb-20 px-4'>
+                    <div className='bg-white rounded-lg shadow-xl w-full max-w-6xl'>
+                        <div className='flex h-full rounded-lg overflow-hidden'>
+                            <Sidebar />
+                            {!selectedUser ? <NoChatSelected /> : <ChatContainer />}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
