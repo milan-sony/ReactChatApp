11e487656a38498f06f983d03a940e9c4103e48c
Milan Sony <milansonyofficial@gmail.com>
2024-12-27 16:36:13 +0530

    feat: messages displayed on page

--- src/components/ChatContainer/ChatContainer.jsx
+++ src/components/ChatContainer/ChatContainer.jsx
@@ -3,9 +3,13 @@ import { userChatStore } from '../../store/userChatStore'
 import Skeleton from '../Skeleton/Skeleton'
 import ChatHeader from '../ChatHeader/ChatHeader';
 import MessageInput from '../MessageInput/MessageInput';
+import { userAuthStore } from '../../store/userAuthStore';
+import defaultProfilePicture from "./avatar.png";
 
 function ChatContainer() {
     const { messages, getMessages, isMessagesLoading, selectedUser } = userChatStore()
+
+    const { authUser } = userAuthStore()
 
     useEffect(() => {
         getMessages(selectedUser._id);
@@ -26,8 +30,37 @@ function ChatContainer() {
             <div className=' flex flex-col w-full overflow-auto'>
                 <ChatHeader />
 
-                <p>Message...</p>
+                {/* message container */}
+                <div className='flex-1 overflow-y-auto p-4 space-y-4'>
+                    {
+                        messages.map((message) => (
+                            <div key={message._id} className={`chat ${message.senderId === authUser._id ? "chat-end" : "chat-start"}`}>
+                                <div className='chat-image avatar'>
+                                    <div className='size-10 rounded-full border'>
+                                        <img src={message.senderId === authUser._id ? authUser.profilePicture || defaultProfilePicture : selectedUser.profilePicture || defaultProfilePicture} alt="Profile Picture" />
+                                    </div>
+                                </div>
+                                <div className="chat-header">
+                                    <time className="text-xs opacity-50">{
+                                        message.createdAt
+                                    }</time>
+                                </div>
+                                <div className='chat-bubble flex'>
+                                    {
+                                        message.image && (
+                                            <image src={message.image} alt="attachment" className='rounded-md mb-2'
+                                            />
+                                        )
+                                    }
+                                    {
+                                        message.text && <p>{message.text}</p>
+                                    }
+                                </div>
+                            </div>
 
+                        ))
+                    }
+                </div>
                 <MessageInput />
             </div>
 
